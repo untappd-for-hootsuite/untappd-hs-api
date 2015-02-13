4f5b75ed7deba67224cc85249461bbddd791f811
Mackenzie Marshall <mack.j.marshall@gmail.com>
2015-02-12 21:48:02 -0800

    Fixed issue of using http to fetch the chekins

--- app/controllers/Checkins.scala
+++ app/controllers/Checkins.scala
@@ -15,7 +15,7 @@ object Checkins extends Controller with AuthTokenManager {
     oAuthToken match {
       case None => Future(Unauthorized(s"Request must include valid ${AuthTokenManager.tokenHeader} header"))
       case Some(authToken) =>
-        val holder: WSRequestHolder = WS.url(s"http://api.untappd.com/v4/checkin/recent")
+        val holder: WSRequestHolder = WS.url(s"https://api.untappd.com/v4/checkin/recent")
           .withQueryString("access_token" -> authToken)
 
         holder.get().map { resp =>
